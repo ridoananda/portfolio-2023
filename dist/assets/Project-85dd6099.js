2e67d136dac23f8b217edb0b369841e352fb15c1
Rido Ananda <ridoananda123@gmail.com>
2023-04-18 07:10:51 +0700

    feat: remove dist ignore folder

---
+++ dist/assets/Project-85dd6099.js
@@ -0,0 +1,1 @@
+import{g as a,h as e,i as c}from"./Project-b0039c61.js";import{c as t,b as s,o as _}from"./index-4d0cf22e.js";const o={class:"pt-12 space-y-20"},f={__name:"Project",setup(r){return(n,m)=>(_(),t("div",o,[s(a),s(e),s(c)]))}};export{f as default};
